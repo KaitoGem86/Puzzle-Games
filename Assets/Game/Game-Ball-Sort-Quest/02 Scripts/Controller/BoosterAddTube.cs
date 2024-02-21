9504770553259dfcc4bfd36d3ba30a30068be15f
TuanDat <letuandat556@gmail.com>
2024-02-21 14:19:20 +0700

    show number of booster, add logic check show ads

--- Assets/Game/Game-Ball-Sort-Quest/02 Scripts/Controller/BoosterAddTube.cs
+++ Assets/Game/Game-Ball-Sort-Quest/02 Scripts/Controller/BoosterAddTube.cs
@@ -1,20 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace BallSortQuest
 {
     public class BoosterAddTube : BoosterController
     {
+        private bool _isOpenAds = false;
+        private void Awake()
+        {
+            ActionEvent.OnResetGamePlay += ResetQuantity;
+        }
+
         public void Start()
         {
-            base.DisplayBooster(PlayerData.UserData.BoosterAddNumber);
+            UpdateBoosterQuantity(true);
         }
 
         public void OnClickAddTube()
         {
+            if(_isOpenAds){
+                Debug.Log("Show ads");
+                //Do something
+            }
+            else{
+                UpdateBoosterQuantity();
+            }
             ActionEvent.OnUserBoosterAdd?.Invoke();
-            DisplayBooster(PlayerData.UserData.BoosterAddNumber);
+        }
+
+        private void ResetQuantity(){
+            if(PlayerData.UserData.HighestLevel < 50){
+                UpdateBoosterQuantity(true);
+            }
+            else{
+                //Do something
+            }
+        }
+
+        private void UpdateBoosterQuantity(bool isReset = false){
+            int quantity = PlayerData.UserData.BoosterAddNumber;
+            if(isReset){
+                quantity = -1;
+            }
+            else{
+                quantity -= 1;
+            }
+            _isOpenAds = quantity < -1;
+            PlayerData.UserData.UpdateValueBooster(this.Type, quantity - PlayerData.UserData.BoosterAddNumber);
+            base.DisplayBooster(PlayerData.UserData.BoosterAddNumber);
+            Debug.Log("Quantity: " + PlayerData.UserData.BoosterAddNumber);
         }
     }
 }
--- Assets/Game/Game-Ball-Sort-Quest/02 Scripts/Controller/BoosterController.cs
+++ Assets/Game/Game-Ball-Sort-Quest/02 Scripts/Controller/BoosterController.cs
@@ -14,19 +14,32 @@ public class BoosterController : MonoBehaviour
     public TypeBooster Type;
     [SerializeField] TMP_Text _quantityText;
     [SerializeField] GameObject _iconAds;
+    [SerializeField] GameObject _iconFree;
 
-    public void DisplayBooster(int value)
+    public virtual void DisplayBooster(int value)
     {
         _quantityText.text = value.ToString();
+        if(value == -1)
+        {
+            _iconAds.SetActive(false);
+            _iconFree.SetActive(true);
+            _quantityText.transform.parent.parent.gameObject.SetActive(false);
+            return;
+        }
         //_quantityText.gameObject.SetActive(true);
         if (value > 0)
         {
             _iconAds.SetActive(false);
+            _iconFree.SetActive(false);
+            _quantityText.transform.parent.parent.gameObject.SetActive(true);
         }
         else
-        {
+        { // use ads when value <= -2
             // _quantityText.gameObject.SetActive(false);
             _iconAds.SetActive(true);
+            _iconFree.SetActive(false);
+            _quantityText.transform.parent.parent.gameObject.SetActive(true);
+            _quantityText.text = "Ads";
         }
     }
 }
