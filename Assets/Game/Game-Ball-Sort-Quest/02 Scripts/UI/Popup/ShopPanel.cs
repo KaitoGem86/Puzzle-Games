0c4d41f42e255fcf7ce9825aecb3993853797a55
TuanDat <letuandat556@gmail.com>
2024-02-29 13:17:13 +0700

    complete add ball purchased button in shop

--- Assets/Game/Game-Ball-Sort-Quest/02 Scripts/UI/Popup/ShopPanel.cs
+++ Assets/Game/Game-Ball-Sort-Quest/02 Scripts/UI/Popup/ShopPanel.cs
@@ -11,6 +11,7 @@ namespace BallSortQuest
         [SerializeField] private Transform _marketNavigateButton;
         [SerializeField] private Transform _tubeNavigateButton;
         [SerializeField] private Transform _backgroundNavigateButton;
+        [SerializeField] private Transform _ballNavigateButton;
         [SerializeField] private TMP_Text _coinText;
         [SerializeField] private GameObject _shopItemPrefab;
 
@@ -26,6 +27,7 @@ namespace BallSortQuest
         private TwoStateElement _marketButton;
         private TwoStateElement _backgroundButton;
         private TwoStateAtMidElement _tubeButton;
+        private TwoStateAtMidElement _ballButton;
 
         private ListShopITemController _listShopITemController;
 
@@ -34,6 +36,7 @@ namespace BallSortQuest
             _marketButton ??= new TwoStateElement(_marketNavigateButton);
             _backgroundButton ??= new TwoStateElement(_backgroundNavigateButton);
             _tubeButton ??= new TwoStateAtMidElement(_tubeNavigateButton);
+            _ballButton ??= new TwoStateAtMidElement(_ballNavigateButton);
             _listShopITemController ??= new ListShopITemController(_backgroundDatas, _tubeDatas, _listViewPort, _shopItemPrefab);
             OnMarketNavigateButtonClick();
             UpdateCoinText();
@@ -48,6 +51,7 @@ namespace BallSortQuest
             _marketButton.SetState(true);
             _backgroundButton.SetState(false);
             _tubeButton.SetState(false, true);
+            _ballButton.SetState(false, true);
             _marketBoard.SetActive(true);
             _listSlideBoard.SetActive(false);
         }
@@ -56,21 +60,35 @@ namespace BallSortQuest
             _marketButton.SetState(false);
             _backgroundButton.SetState(false);
             _tubeButton.SetState(true, true);
+            _ballButton.SetState(false, true);
             _marketBoard.SetActive(false);
             _listSlideBoard.SetActive(true);
             _listShopITemController.ClearBoard();
             _listShopITemController.ShowListItem(_tubeDatas);
         }
 
+        public void OnBallNavigateButtonClick(){
+            _marketButton.SetState(false);
+            _backgroundButton.SetState(false);
+            _tubeButton.SetState(false, false);
+            _ballButton.SetState(true, true);
+            _marketBoard.SetActive(false);
+            _listSlideBoard.SetActive(true);
+            //_listShopITemController.ClearBoard();
+            //_listShopITemController.ShowListItem(_backgroundDatas);
+        }
+
         public void OnBackgroundNavigateButtonClick(){
             _marketButton.SetState(false);
             _backgroundButton.SetState(true);
             _tubeButton.SetState(false, false);
+            _ballButton.SetState(false, false);
             _marketBoard.SetActive(false);
             _listSlideBoard.SetActive(true);
             _listShopITemController.ClearBoard();
             _listShopITemController.ShowListItem(_backgroundDatas);
         }
+
 
         public void UpdateCoinText(){
             _coinText.text = PlayerData.UserData.CoinNumber.ToString();
