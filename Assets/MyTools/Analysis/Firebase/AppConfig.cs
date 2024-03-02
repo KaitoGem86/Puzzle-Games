6e2858c2615f25ad699c230195056d360e01a193
TuanDat <letuandat556@gmail.com>
2024-03-02 11:48:33 +0700

    add app config

--- Assets/MyTools/Analysis/Firebase/AppConfig.cs
+++ Assets/MyTools/Analysis/Firebase/AppConfig.cs
@@ -6,15 +6,58 @@ using UnityEngine;
 
 public class AppConfig : SingletonMonoBehaviour<AppConfig>
 {
-    public override void Awake()
-    {
-        SetDefaultConfigValue();
-        base.Awake();
-    }
+    [Header("~~~~~VALUE DEFAULT~~~~~")]
+        [SerializeField] int _bannerAdLevel;
+        [SerializeField] int _interAdLevel;
+        [SerializeField] int _interFrequencyTime;
+        [SerializeField] bool _isShowInterWithClosePopupRemoveAds;
+        [Space(10)]
+        [Header("~~~~~~~VALUE IS FETCH~~~~~~")]
+        [SerializeField] string ValueIsFetch;
 
-    public void SetDefaultConfigValue()
-    {
+        public int BannerAdLevel
+        {
+            get => _bannerAdLevel;
+            set
+            {
+                _bannerAdLevel = value;
+                AppendValueIsFetch(value.ToString());
+            }
+        }
 
-    }
+        public int InterAdLevel
+        {
+            get => _interAdLevel;
+            set
+            {
+                _interAdLevel = value;
+                AppendValueIsFetch(value.ToString());
+            }
+        }
+
+        public int InterFrequencyTime
+        {
+            get => _interFrequencyTime;
+            set
+            {
+                _interFrequencyTime = value;
+                AppendValueIsFetch(value.ToString());
+            }
+        }
+
+        public bool IsShowInterWithClosePopupRemoveAds
+        {
+            get => _isShowInterWithClosePopupRemoveAds;
+            set
+            {
+                _isShowInterWithClosePopupRemoveAds = value;
+                AppendValueIsFetch(value.ToString());
+            }
+        }
+
+        private void AppendValueIsFetch(string value)
+        {
+            ValueIsFetch += $"|{value}|";
+        }
 }
 
