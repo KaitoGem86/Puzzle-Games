809f18cdd9d79a9c5da04a390430fd75ec2afcb8
TuanDat <letuandat556@gmail.com>
2024-03-06 15:35:58 +0700

    complete change UI in GamePlay scene

--- Assets/Game/02 SCRIPTS/GameManager/GameUIManager.cs
+++ Assets/Game/02 SCRIPTS/GameManager/GameUIManager.cs
@@ -6,7 +6,6 @@ public class GameUIManager : SingletonMonoBehaviour<GameUIManager>
 {
     [SerializeField] private TMP_Text levelText;
     [SerializeField] private BackgroundController _backgroundController;
-    [SerializeField] private TMP_Text _labelText;
     [SerializeField] private GameObject _safeAreaCanvas;
     [SerializeField] private TMP_Text _tutorialText;
     [SerializeField] private GameObject _closeButton;
@@ -55,91 +54,102 @@ public class GameUIManager : SingletonMonoBehaviour<GameUIManager>
         BallSortQuest.GameManager.Instance.StateGameController.OnMenu();
     }
 
-    public void OpenCloseChallengeModePopup(){
+    public void OpenCloseChallengeModePopup()
+    {
         PopupSystem.PopupManager.CreateNewInstance<PopupCloseChallengeMode>().Show("Bạn có muốn bỏ cuộc không?", TypeChallenge.None);
     }
 
-    public void OpenRewardPopup(){
+    public void OpenRewardPopup()
+    {
         PopupSystem.PopupManager.CreateNewInstance<PopupGoldReward>().Show(TimeFromToGoogle.Instance.Now());
     }
 
-    public void OpenResetGameModePopup(){
+    public void OpenRankingPopup()
+    {
+        PopupSystem.PopupManager.CreateNewInstance<NotificationPopup>().Show("Chức năng đang phát triển");
+    }
+
+    public void OpenResetGameModePopup()
+    {
         PopupSystem.PopupManager.CreateNewInstance<PopupCloseChallengeMode>().Show("Dữ liệu game sẽ bị mất, bạn có muốn tiếp tục không?", BallSortQuest.GameManager.Instance.GameModeController.CurrentGameMode);
     }
 
-    public void OpenMenuPopup(){
+    public void OpenMenuPopup()
+    {
         PopupSystem.PopupManager.CreateNewInstance<MenuPanel>().Show();
     }
 
     public void UpdateLevelText()
     {
-        if(BallSortQuest.GameManager.Instance.GameModeController.CurrentGameMode == TypeChallenge.None)
-            levelText.text = BallSortQuest.GameManager.Instance.Level.level.ToString();
+        if (BallSortQuest.GameManager.Instance.GameModeController.CurrentGameMode == TypeChallenge.None)
+            levelText.text = "Level " + BallSortQuest.GameManager.Instance.Level.level.ToString();
     }
 
-    
+
 
     /// <summary>
     /// Set particle size follow camera size. Default size is setted when camera size is 5
     /// </summary>
     /// <param name="cameraSize"></param>
-    public void SetParticleSize(float cameraSize){
+    public void SetParticleSize(float cameraSize)
+    {
         InitializeParticleSizes();
         // _topParticle.transform.localScale  = cameraSize / 5 * _originalTopParticleScale;
         // _leftParticle.transform.localScale = cameraSize / 5 * _originalLeftParticleScale;
         // _rightParticle.transform.localScale = cameraSize / 5 * _originalRightParticleScale;
     }
 
-    private void InitializeParticleSizes() {
-    _originalTopParticleScale = _topParticle.transform.localScale;
-    _originalLeftParticleScale = _leftParticle.transform.localScale;
-    _originalRightParticleScale = _rightParticle.transform.localScale;
+    private void InitializeParticleSizes()
+    {
+        _originalTopParticleScale = _topParticle.transform.localScale;
+        _originalLeftParticleScale = _leftParticle.transform.localScale;
+        _originalRightParticleScale = _rightParticle.transform.localScale;
     }
 
-    public void PlayBorderParticle(){
+    public void PlayBorderParticle()
+    {
         _topParticle.Play();
         _leftParticle.Play();
         _rightParticle.Play();
     }
 
-    private void ResetGamePlayWithMode(){
+    private void ResetGamePlayWithMode()
+    {
         switch (BallSortQuest.GameManager.Instance.GameModeController.CurrentGameMode)
         {
             case TypeChallenge.None:
-                if(BallSortQuest.GameManager.Instance.Level.level == 1)
+                if (BallSortQuest.GameManager.Instance.Level.level == 1)
                 {
                     _safeAreaCanvas.SetActive(false);
                     _tutorialText.text = "Vui lòng chọn nước để đổ vào chai thủy tinh";
                     return;
                 }
                 _safeAreaCanvas.SetActive(true);
-                if(BallSortQuest.GameManager.Instance.Level.level == 2)
+                if (BallSortQuest.GameManager.Instance.Level.level == 2)
                 {
+                    _safeAreaCanvas.SetActive(false);
                     _tutorialText.text = "Bạn có thể đổ nước lên trên màu giống nhau";
                 }
-                else{
+                else
+                {
                     _tutorialText.gameObject.SetActive(false);
                 }
                 SetUpGamePlayScene(false);
-                _labelText.text = "Level";
-                _labelText.gameObject.SetActive(true);
-                levelText.fontSize = 64;
+                levelText.fontSize = 72;
                 break;
             case TypeChallenge.Hidden:
                 SetUpGamePlayScene(true);
-                _labelText.gameObject.SetActive(false);
                 levelText.text = "HIDDEN";
+                levelText.fontSize = 72;
                 break;
             case TypeChallenge.Move:
                 SetUpGamePlayScene(true);
-                _labelText.gameObject.SetActive(false);
                 levelText.text = "";
                 levelText.fontSize = 100;
                 BallSortQuest.GameManager.Instance.GameModeController.MoveModeController.SetMoveText(levelText);
                 break;
             case TypeChallenge.Timer:
                 SetUpGamePlayScene(true);
-                _labelText.gameObject.SetActive(false);
                 levelText.text = "";
                 levelText.fontSize = 100;
                 BallSortQuest.GameManager.Instance.GameModeController.TimerModeController.SetTimerText(levelText);
@@ -147,13 +157,15 @@ public class GameUIManager : SingletonMonoBehaviour<GameUIManager>
         }
     }
 
-    private void SetUpGamePlayScene(bool isChallenge){
+    private void SetUpGamePlayScene(bool isChallenge)
+    {
         _menuButton.SetActive(!isChallenge);
         _challengeButton.SetActive(!isChallenge);
         _closeButton.SetActive(isChallenge);
     }
 
-    private void InitBackground(){
+    private void InitBackground()
+    {
         _backgroundController.InitBackground();
     }
 }
